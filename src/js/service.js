fe6d754f15ce0d128649b4d1626f25e0c4661349
Sergey Tomilov <78944939+TomSG03@users.noreply.github.com>
2021-10-03 23:19:08 +0800

    Update service.js

--- src/js/service.js
+++ src/js/service.js
@@ -1,5 +1,5 @@
 export default function swichPhoneNumber(PhoneNumber) {
-  let newNumber = PhoneNumber.replace(/(\s|-|\(|\))/g, '').replace(/^(7|86)/, '+$&').replace(/^8\d{10}$/, (str) => str.replace(/^8/, '+7'));
+  const newNumber = PhoneNumber.replace(/(\s|-|\(|\))/g, '').replace(/^(7|86)/, '+$&').replace(/^8\d{10}$/, (str) => str.replace(/^8/, '+7'));
 //   let newNumber = PhoneNumber.replace(/(\s|-|\(|\))/g, '');
 //   newNumber = newNumber.replace(/^(7|86)/, '+$&');
 //   newNumber = newNumber.replace(/^8\d{10}$/, (str) => str.replace(/^8/, '+7'));
